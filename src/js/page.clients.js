7064da8e413169ca9677010ef3471aade2572157
Alhadis <gardnerjohng@gmail.com>
2015-06-26 14:47:57 +1000

    Extend sizing of manual clipping mask by 2px
    The outermost borders of the client-grid are sitting outside the borders
    of its content, so a few extra pixels of space are needed to ensure it's
    not cutting off the bottom border.

--- src/js/page.clients.js
+++ src/js/page.clients.js
@@ -18,13 +18,13 @@
 
 
 		/** If the scrollHeight of the main element appears to be taller than three rows in height on desktop, something went wrong. */
-		if(WIN.innerWidth > 599 && main.scrollHeight > ((cellSize * 3) + top)){
+		if(WIN.innerWidth > 599 && main.scrollHeight > cellSize * 3 + top){
 
 			clip = clients.parentNode.insertBefore(New("div", {className: "clipping-mask"}), clients);
 			clip.appendChild(clients);
 
 			WIN.addEventListener("resize", (new function(e){
-				clip.style.height	=	(cell.offsetWidth * 2 + top)+"px"
+				clip.style.height	=	(cell.offsetWidth * 2 + top + 2)+"px"
 				return this.constructor;
 			}).debounce(150));
 		}
